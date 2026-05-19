ac4355dd68efa2b3309f3a8257269e2b627f60b2
Hawtian Wang <twistoy.wang@gmail.com>
2026-05-19 14:38:06 +0800

    feat: port pi-tool-renderer extension, fix user message border wrapping

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent"
 import registerZai from "./providers/zai/index.js"
 import { registerPrompts } from "./prompts/index.js"
-import initSubagents from "./subagents/index.js"
+import { initExtensions } from "./extensions/index.js"
 
 export default function (pi: ExtensionAPI) {
   registerZai(pi)
   registerPrompts(pi)
-  initSubagents(pi)
+  initExtensions(pi)
 }
